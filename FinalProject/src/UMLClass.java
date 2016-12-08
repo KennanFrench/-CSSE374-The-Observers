6096202d4c878e9814bffefb9bee580ab58173ab
Joseph Brown <brownje@rose-hulman.edu>
2016-12-07 22:43:27 -0500

    Added the classes

---
+++ FinalProject/src/UMLClass.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class UMLClass {
+	private String name;
+	private Category category;
+	private ArrayList<UMLField> categories;
+	private ArrayList<UMLMethod> methods;
+	
+	public UMLClass()
+	{
+		
+	}
+
+}
