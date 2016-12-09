085d84ad9a4ff179e513940a0d16cf07d31bc619
Joseph Brown <brownje@rose-hulman.edu>
2016-12-09 14:55:21 -0500

    Added fields to UML

--- FinalProject/src/UMLClass.java
+++ FinalProject/src/UMLClass.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class UMLClass {
 	private String name;
 	private Category category;
-	private ArrayList<UMLField> categories;
+	private ArrayList<UMLField> fields;
 	private ArrayList<UMLMethod> methods;
 	
 	public UMLClass()
